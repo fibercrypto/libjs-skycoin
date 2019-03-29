11ed2d17098a83aec199f195bc921f0219241829
cbermudez97 <cbermu.97@gmail.com>
2019-03-29 16:00:03 -0400

    [skyapi] refs #12 - Add test for the node skycoin api

--- dev/tester/index.js
+++ dev/tester/index.js
@@ -1,21 +1,32 @@
 "use strict";
 
 // Set BASEPATH
-const localBasePath = 'http://127.0.0.1:6420';
 const developBasePath = 'https://staging.node.skycoin.net';
 const prodBasePath = 'https://node.skycoin.net';
 
 // AXIOS
 const testAxios = function (basePath) {
-    const skyapi_axios = require("libsky-axios"); //Load the Axios API client
-    let api_axios_config = new skyapi_axios.Configuration(); //Create a default configuration class
+    const skyapi_axios = require("libsky-axios"); // Load the Axios API client
+    let api_axios_config = new skyapi_axios.Configuration(); // Create a default configuration class
     let api_axios = new skyapi_axios.DefaultApi(api_axios_config, basePath); // Create the API instance
-    // Make a petition to version of skycoin node
+    // Make a petition of the version to the skycoin node
     api_axios.version().then(result => {
-        console.log(`Response from skyapi_axios\n${basePath}: ${JSON.stringify(result.data)}`); //Print the response data field
+        console.log(`Response from skycoin node in ${basePath} using axios client: ${JSON.stringify(result.data)}`); //Print the response data field
     });
 };
 
-// testAxios(localBasePath);
+// NODE
+const testNode = function (basePath) {
+    const skyapi_node = require("libsky-node"); // Load the Node API client
+    let api_node = new skyapi_node.DefaultApi(basePath); // Create the API instance
+    // Make a petition of the version to the skycoin node
+    api_node.version().then(result => {
+        console.log(`Response from skycoin node in ${basePath} using node client: ${JSON.stringify(result.body)}`); //Print the response body field
+    });
+};
+
 testAxios(developBasePath);
 testAxios(prodBasePath);
+
+testNode(developBasePath);
+testNode(prodBasePath);
