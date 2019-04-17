c941fc902d049ff2c35c15b8b0baca83d5bb0a40
stdevCbermudez <cbermu.97@gmail.com>
2019-04-17 12:19:33 -0400

    [skyapi] refs #12 - Modify nodejs api client tester

--- tests/node/test/node.js
+++ tests/node/test/node.js
@@ -1,42 +1,30 @@
 "use strict";
 
-const localBasePath = 'http://localhost:6420/';
-const developBasePath = 'https://staging.node.skycoin.net';
-const prodBasePath = 'https://node.skycoin.net';
+const config = require('../../config');
 
 let nodeClient = require('libsky-node');
 var expect = require('chai').expect;
+
+const checkVersion = function (path) {
+    context(`Getting version from ${path}.`, function() {
+        it('Should return version object', function() {
+            let api = new nodeClient.DefaultApi(path);
+            return api.version().then(result => {
+                expect(result).to.be.a('object');
+                expect(result).to.have.property('body');
+                expect(result.body).to.be.a('object');
+                expect(result.body).to.have.property('version');
+                expect(result.body).to.have.property('commit');
+                expect(result.body).to.have.property('branch');
+            });
+        });
+    });
+};
 
 describe('#nodeClient', function() {
 
     this.timeout(10000);
 
-    context(`Getting version from ${developBasePath}.`, function() {
-        it('Should return version object', function() {
-            let api = new nodeClient.DefaultApi(developBasePath);
-            return api.version().then(result => {
-                expect(result).to.be.a('object');
-                expect(result).to.have.property('body');
-                expect(result.body).to.be.a('object');
-                expect(result.body).to.have.property('version');
-                expect(result.body).to.have.property('commit');
-                expect(result.body).to.have.property('branch');
-            });
-        });
-    });
-
-    context(`Getting version from ${prodBasePath}.`, function() {
-        it('Should return version object', function() {
-            let api = new nodeClient.DefaultApi(prodBasePath);
-            return api.version().then(result => {
-                expect(result).to.be.a('object');
-                expect(result).to.have.property('body');
-                expect(result.body).to.be.a('object');
-                expect(result.body).to.have.property('version');
-                expect(result.body).to.have.property('commit');
-                expect(result.body).to.have.property('branch');
-            });
-        });
-    });
+    checkVersion(config.HOST);
 
 });
