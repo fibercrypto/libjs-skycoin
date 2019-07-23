8bc5a88eff07edfd771af16b821f19b6aff7c587
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-23 14:59:45 -0400

    [travis] refs #21 Added `node-gyp`

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -3,6 +3,8 @@ set -ex
 sudo apt-get update
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
-sudo apt install gcc libpcre3-dev g++ nodejs-dev  -y
+sudo apt install gcc libpcre3-dev g++ -y
 
 mkdir swig_build && cd swig_build && wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && tar -zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && sudo ./configure --prefix=/usr && sudo make && sudo make install && cd ../../ && sudo rm -rf swig_build
+
+sudo npm install -g node-gyp
--- lib/skycoin/binding.gyp
+++ lib/skycoin/binding.gyp
@@ -4,6 +4,7 @@
       "target_name": "skycoin",
       "sources": ["libskycoin_wrap.c" ],
       "include_dirs": ["../../gopath/src/github.com/skycoin/libskycoin/include", ".", "/usr/include"],
+      "cflags": ["-lv8"],
     }
   ]
 }
