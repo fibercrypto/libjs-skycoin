b4ff169c705aa792cb8e09c77f27580778a15778
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-23 15:04:31 -0400

    [travis] refs #21 Correcting declaration in npm

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -7,4 +7,4 @@ sudo apt install gcc libpcre3-dev g++ -y
 
 mkdir swig_build && cd swig_build && wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && tar -zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && sudo ./configure --prefix=/usr && sudo make && sudo make install && cd ../../ && sudo rm -rf swig_build
 
-sudo npm install -g node-gyp
+npm install -g node-gyp
